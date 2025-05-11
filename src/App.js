c8bbfc23aba3c336b97b66d8d6426687ab49e013
Rosie1028 <rosangelahs@icloud.com>
2025-05-10 19:54:54 -0500

    l

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import BootstrapTextarea from './BootstrapTextArea';
-import Footer from './components/Footer';
- 
-function App() {
+import ButterflyLogo from './components/ButterflyLogo';
+
+const App = () => {
   return (
     <div className="App">
-      <Container fluid>
-        <br/>
-        <Row>
-          <Col sm="2">
+      <Container fluid className="app-container">
+        <Row className="header-section">
+          <Col lg={{ span: 6, offset: 1 }} className="header-content">
+            <div className="header-container">
+              <h1 className="app-title">
+                <span className="title-line">NaSumm</span>
+              </h1>
+              <p className="app-subtitle">
+                Transforma noticias en resúmenes concisos y claros
+              </p>
+            </div>
           </Col>
-          <Col sm="6">
-           <h2 className='App-header'>Sumarizador de noticias &#127464;&#127482;</h2>
+          <Col lg={4} className="header-decoration">
+            <div className="decoration-circle"></div>
           </Col>
-          
         </Row>
-        <br/>
-        <Row>
-        <Col sm="2">
-        </Col>
-        <Col>
-        <BootstrapTextarea/>
         
-        </Col>
-        <Col sm="2">
-        </Col>
+        <Row className="content-section">
+          <Col lg={{ span: 6, offset: 1 }}>
+            <div className="content-card">
+              <BootstrapTextarea />
+            </div>
+          </Col>
+          <Col lg={4} className="animation-column">
+            <div className="animation-container">
+              <div className="floating-elements">
+                <div className="floating-circle circle-1"></div>
+                <div className="floating-circle circle-2"></div>
+                <div className="floating-circle circle-3"></div>
+                <div className="floating-dot dot-1"></div>
+                <div className="floating-dot dot-2"></div>
+                <div className="floating-dot dot-3"></div>
+              </div>
+            </div>
+          </Col>
         </Row>
-        <br/><br/><br/>
-        <Row>
-          <Footer/>
+        
+        <Row className="mt-5">
+          <Col>
+            <ButterflyLogo />
+          </Col>
         </Row>
       </Container>
     </div>
   );
-}
+};
 
 export default App;
